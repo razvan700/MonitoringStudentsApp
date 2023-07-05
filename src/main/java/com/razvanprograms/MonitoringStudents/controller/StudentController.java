1c5cf4ad8ee74f2587594cee1e8657c2bde4e59c
razvan700 <110129667+razvan700@users.noreply.github.com>
2023-07-05 22:15:33 +0300

    Added functionality to rest controller
    Added add method

--- src/main/java/com/razvanprograms/MonitoringStudents/controller/StudentController.java
+++ src/main/java/com/razvanprograms/MonitoringStudents/controller/StudentController.java
@@ -1,4 +1,23 @@
 package com.razvanprograms.MonitoringStudents.controller;
 
+import com.razvanprograms.MonitoringStudents.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import com.razvanprograms.MonitoringStudents.model.Student;
+@RestController
+@RequestMapping("/student")
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student){
+        studentService.saveStudent(student);
+        return "A new student has been added!";
+    }
+
 }
--- src/main/java/com/razvanprograms/MonitoringStudents/service/StudentServiceImpl.java
+++ src/main/java/com/razvanprograms/MonitoringStudents/service/StudentServiceImpl.java
@@ -1,4 +1,17 @@
 package com.razvanprograms.MonitoringStudents.service;
 
-public class StudentServiceImpl {
+import com.razvanprograms.MonitoringStudents.model.Student;
+import com.razvanprograms.MonitoringStudents.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
 }
