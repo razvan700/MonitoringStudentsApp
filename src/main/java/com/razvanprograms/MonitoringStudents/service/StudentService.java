ff16501a2088a77c72a878675238a25a1b5f99c4
razvan700 <110129667+razvan700@users.noreply.github.com>
2023-07-05 21:37:55 +0300

    save student
    Implemented the method that saves a student

--- src/main/java/com/razvanprograms/MonitoringStudents/service/StudentService.java
+++ src/main/java/com/razvanprograms/MonitoringStudents/service/StudentService.java
@@ -1,4 +1,5 @@
 package com.razvanprograms.MonitoringStudents.service;
-
+import com.razvanprograms.MonitoringStudents.model.Student;
 public interface StudentService {
+    public Student saveStudent(Student student);
 }
