b72656c20ccc00c243672413db14790801e4bba0
razvan700 <110129667+razvan700@users.noreply.github.com>
2023-07-11 16:15:29 +0300

    Styled frontend, created button for adding students
    Styled frontend, created button for adding students, starting to introduce students into database

--- monitoringfrontend/src/App.js
+++ monitoringfrontend/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Student from './components/Student'
 import AppBar from './components/Appbar'
 function App() {
   return (
     <div className="App">
       <AppBar/>
+      <Student/>
     </div>
   );
 }
---
+++ monitoringfrontend/src/components/Student.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+export default function Student() {
+  const paperStyle = {padding:'50px 20px', width:600, margin:"20px auto"}
+  const [name, setName]=useState('')
+  const [address, setAddress]=useState('')
+  return (
+    
+    <Container>
+      <Paper elevation={3} style = {paperStyle}>
+        <h1 style={{color:"green"}}><u> Add student</u></h1>
+    <Box
+        
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1 },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="standard-basic" label="Student name" variant="standard" fullWidth
+      value = {name}
+      onChange ={(e)=>setName(e.target.value)}
+      />
+      <TextField id="standard-basic" label="Student address" variant="standard" fullWidth
+      value = {address}
+      onChange = {(e)=>setAddress(e.target.value)}
+      />
+    </Box>
+    <Button variant="contained" color="success">
+      Success
+    </Button>
+    </Paper>
+    </Container>
+  );
+}
